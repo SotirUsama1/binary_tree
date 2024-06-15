54256c292fbab893e0266328f5661a12307d3c8b
sotir <sotirusama16@gmail.com>
2024-06-15 04:22:24 +0300

    task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,24 +8,24 @@
  * Return: pointer to the child node created
  */
 
-binary_tree_t* binary_tree_insert_right(binary_tree_t* parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t* new;
+	binary_tree_t *new;
 
-    if (!parent)
-        return (NULL);
+	if (!parent)
+		return (NULL);
 
-    new = malloc(sizeof(binary_tree_t));
-    if (!new)
-        return (NULL);
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
 
-    new->n = value;
-    new->parent = parent;
-    new->left = NULL;
-    new->right = parent->right;
-    parent->right = new;
-    if (new->right)
-        new->right->parent = new;
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = parent->right;
+	parent->right = new;
+	if (new->right)
+		new->right->parent = new;
 
-    return (new);
+	return (new);
 }
