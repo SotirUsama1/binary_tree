e911bc13a06af907a83d42f5fc0ca48015356cc1
sotir <sotirusama16@gmail.com>
2024-06-15 05:01:26 +0300

    task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_delete - delete a tree
+* @tree: pointer to the tree's root
+*/
+void binary_tree_delete(binary_tree_t* tree) {
+	if (tree == NULL)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
