3fd0519fe5f74f8d79c726e03f3a1734505031e3
sotir <sotirusama16@gmail.com>
2024-06-18 08:04:28 +0300

    task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,7 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t hr = -1;
 
 	if (!tree)
-		return (-1);
+		return (0);
 
 	hl = 1 + binary_tree_height(tree->left);
 	hr = 1 + binary_tree_height(tree->right);
